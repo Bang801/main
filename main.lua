41060c9454a77d6fe64c1d2081a4bc0096bb1e22
Bang801 <jovs3781@gmail.com>
2024-12-31 18:33:17 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local gameId = game.PlaceId
     
-if gameId == 6137321701 then
+if gameId == 6137321701 or gameId == 6348640020 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Bang801/games/refs/heads/main/blair.lua"))()
 elseif gameId == 1962086868 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Bang801/games/refs/heads/main/TOH.lua"))()
