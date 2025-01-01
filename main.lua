9c65c2c55dc07b40efb5f1d2e4df02b127c7a941
Bang801 <jovs3781@gmail.com>
2025-01-01 18:11:24 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,4 +6,6 @@ elseif gameId == 1962086868 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Bang801/games/refs/heads/main/TOH.lua"))()
 elseif gameId == 8260276694 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Bang801/games/refs/heads/main/Abiltywars.lua"))()
+else
+    game.Players.LocalPlayer:Kick("[RizzHub] This game is not currently supported by RizzHub. Please visit our supported games list to see compatible games. Universal script support coming soon!")
 end 
