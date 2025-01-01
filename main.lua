a0302bb9045624ab86c3beae5dded57fb8b697c4
Bang801 <jovs3781@gmail.com>
2025-01-01 15:34:09 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,4 +4,6 @@ if gameId == 6137321701 or gameId == 6348640020 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Bang801/games/refs/heads/main/blair.lua"))()
 elseif gameId == 1962086868 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Bang801/games/refs/heads/main/TOH.lua"))()
+elseif gameId == 8260276694 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Bang801/games/refs/heads/main/Abiltywars.lua"))()
 end 
